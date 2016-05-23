cf4d076431db81f791b38170bb1cb01553adb53e
Wasili <b.j.prattis@gmail.com>
2016-05-23 15:38:13 +0200

    Added unityman project
    Nothing in the scene yet.
    SnowBall class ported.
    GameObject class ported

---
+++ unityman/Assets/MyGameObject.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MyGameObject : MonoBehaviour {
+	private string _name;
+    private bool _isDeleted;
+
+    void Awake()
+    {
+        _name = name;
+	    _isDeleted = false;
+    }
+
+    void Start()
+    {
+        initialize();
+    }
+
+    void Update()
+    {
+        update(Time.deltaTime);
+    }
+    
+    void initialize()
+    {
+        //Start-up logic is specified in the inheriting object
+    }
+
+    void update(float deltaTime)
+    {
+        //Per-frame logic is specified in the inheriting object
+    }
+
+    void renderGUI()
+    {
+        //Per-frame GUI behaviour is specified in the inheriting object
+    }
+
+    void Destroy()
+    {
+        _isDeleted = true;
+    }
+
+    string getName()
+    {
+        return _name;
+    }
+
+    GameObject getSceneNode()
+    {
+        return gameObject;
+    }
+
+    bool isMarkedForDeletion()
+    {
+        return _isDeleted;
+    }
+}
---
+++ unityman/Assets/SnowBall.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SnowBall : MyGameObject {
+    private float _speed;
+    private float _snowballLifetime;
+    private Vector3 _velocity;
+    private Vector3 _targetPosition;
+    private EnemyAI[] _enemies;
+
+    void Awake()
+    {
+        _speed = 10.0f;
+        _snowballLifetime = 1.3f;
+        _velocity = (_targetPosition - transform.position).normalized;
+
+        _enemies = FindObjectsOfType<EnemyAI>();
+    }
+
+    void update(float deltaTime)
+    {
+        float _deltaTime = deltaTime;
+        _snowballLifetime -= _deltaTime;
+        transform.position = transform.position + (_velocity * _deltaTime * _speed);
+
+        for (int i = 0; i < _enemies.Length; i++)
+        {
+            /*if (_enemies[i] != null)
+            {
+                if (!_enemies[i].isMarkedForDeletion())
+                {
+                    if (Vector3.Distance(transform.position, _enemies[i].transform.position) <= 2.0f)
+                    {
+                        Destroy(_enemies[i]);
+                        Destroy(this);
+                    }
+                }
+            }*/
+        }
+
+        if (_snowballLifetime <= 0)
+        {
+            Destroy(this);
+        }
+    }
+}

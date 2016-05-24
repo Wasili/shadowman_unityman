9c1a0a57d2882ad1ebed50eaaba537f97a11c357
Wasili <wasili.prattis@hva.nl>
2016-05-24 18:12:01 +0200

    Added working directional light with damage

--- unityman/Assets/Scripts/DynamicLightSource.cs
+++ unityman/Assets/Scripts/DynamicLightSource.cs
@@ -16,8 +16,7 @@ public class DynamicLightSource : MyGameObject
     const float _baseRotationSpeed = 10.0f;
     float _rotationSpeed;
     float _movementSpeed;
-
-    public Transform target;
+    
     private bool isDamaging;
     void Awake()
     {
@@ -37,11 +36,11 @@ public class DynamicLightSource : MyGameObject
         //  _targetPosition = _player.transform.position;
         _lastKnownPlayerPos = _player.transform.position;
     }
-
+    
     void Update()
     {
         _damagePlayer();
-        isDamaging = true;
+        //float angle = Quaternion.Angle(Quaternion.Euler(new Vector3(0, 0, 0)), transform.rotation);
         //_parentNode->setRotation(vector3df(_xRotation, _yRotation, _zRotation));
         if (isDamaging)
         {
@@ -58,20 +57,26 @@ public class DynamicLightSource : MyGameObject
             {
                 _parentNode.transform.position = (_parentNode.transform.position + ((target.position - _parentNode.transform.position).normalized * _movementSpeed * Time.deltaTime));
             }*/
+            _searchTimer = _searchCooldown;
+            //Vector3 target = new Vector3(_player.transform.position.x, _parentNode.transform.position.y, _player.transform.position.z);
+            //transform.LookAt(target);
         }
         //search
         else
         {
+            _searchTimer -= Time.deltaTime;
+            if (_searchTimer <= 0)
+            {
+                if ((_targetRotation - _parentNode.transform.eulerAngles).magnitude > 1)
+                {
+                    _parentNode.transform.eulerAngles = (_parentNode.transform.eulerAngles + ((_targetRotation - _parentNode.transform.eulerAngles).normalized * _rotationSpeed * Time.deltaTime));
+
+                }
+            }
             // _parentNode.transform.Rotate(new Vector3(0, Time.deltaTime * _rotationSpeed, 0));
             //newRotationY += 5.0f * Time.deltaTime;
             //transform.rotation = new Quaternion(transform.rotation.x + 30, newRotationY, transform.rotation.z, transform.rotation.w);
         }
-        if ((_targetRotation - _parentNode.transform.eulerAngles).magnitude > 1)
-        {
-            _parentNode.transform.eulerAngles = (_parentNode.transform.eulerAngles + ((_targetRotation - _parentNode.transform.eulerAngles).normalized * _rotationSpeed * Time.deltaTime));
-
-        }
-        Debug.Log(transform.eulerAngles);
         /*if ((_targetRotation - _parentNode.transform.eulerAngles).magnitude > 1)
          {
              _parentNode.transform.eulerAngles = (_parentNode.transform.eulerAngles + ((_targetRotation - _parentNode.transform.eulerAngles).normalized * _rotationSpeed * Time.deltaTime));
@@ -85,31 +90,26 @@ public class DynamicLightSource : MyGameObject
     void _damagePlayer()
     {
         //create a ray with its starting point being the mouse cursor. This method automatically converts screen coordindates to world coordinates if the correct camera is provided
-        Ray ray = new Ray(transform.position, target.transform.position);
+        Ray ray = new Ray(_player.transform.position, transform.position);
         RaycastHit hit;
-
-        if (Physics.Linecast(transform.position, target.position, out hit))
+        if (Physics.Raycast(ray, out hit, 100.0f))
         {
-            if (hit.transform.gameObject.tag == "Player")
+            isDamaging = false;
+            _searchTimer -= Time.deltaTime;
+            if (_searchTimer <= 0)
             {
-                isDamaging = true;
-                _player.updateHealth(_damage * Time.deltaTime);
-                //_targetRotation = _parentNode.transform.eulerAngles;
-                //    _targetPosition = _player.transform.position;
-                _searchTimer = _searchCooldown;
-                _rotationSpeed = (_baseRotationSpeed * 2.0f) - ((_baseRotationSpeed * _player.getHealthPercentage() / 100.0f));
+                //   _targetRotation = _parentNode.transform.eulerAngles;
+                _targetRotation.y += 5;
             }
-            else
-            {
-                isDamaging = false;
-                _searchTimer -= Time.deltaTime;
-                if (_searchTimer <= 0)
-                {
-                    //   _targetRotation = _parentNode.transform.eulerAngles;
-                    _targetRotation.y += 5;
-                }
                 //  _targetPosition = _parentNode.transform.position;
-            }
+        }
+        else {
+            isDamaging = true;
+            _player.updateHealth(_damage * Time.deltaTime);
+            //_targetRotation = _parentNode.transform.eulerAngles;
+            //    _targetPosition = _player.transform.position;
+            _searchTimer = _searchCooldown;
+            _rotationSpeed = (_baseRotationSpeed * 2.0f) - ((_baseRotationSpeed * _player.getHealthPercentage() / 100.0f));
         }
 
     }
